cc1713d51f1d657b27826f546ffce8e61b4ee98e
FelipePacheco <felipepace09@gmail.com>
2021-08-24 20:00:26 -0600

    Ey

--- pages/api/ElEndpoint.js
+++ pages/api/ElEndpoint.js
@@ -1,3 +1,20 @@
-export default function handler(req, res) {
-  res.status(200).json({ Soy: 'El Endpoint' });
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+const applyCors = (handler) => (req, res) => {
+  cors(req, res, (result) => {
+    if (result instanceof Error) {
+      res.status(500).json({ Soy: 'Error :c' });
+    }
+    return handler(req, res);
+  });
 }
+
+export async function handler(req, res) {
+  return res.status(200).json({ Soy: 'El Endpoint' });
+}
+
+export default applyCors(handler);
